d33dd5676b95d34b1742318009eeaea08e38fe95
Subham Kr Gupta <subham.gupta@motivitylabs.com>
2023-06-27 15:40:09 +0530

    entity upload

--- blog-microservice/src/main/java/com/blog/entity/BlogDetails.java
+++ blog-microservice/src/main/java/com/blog/entity/BlogDetails.java
@@ -1,6 +1,7 @@
 package com.blog.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
@@ -18,11 +19,17 @@ public class BlogDetails {
 
     @Id
     private String id;
+    @NotNull
     private String author;
+    @NotNull
     private String title;
+    @NotNull
     private String topic;
+    @NotNull
     private String email;
+    @NotNull
     private String blogText;
+    @NotNull
     private String imagePath;
     private List<String> hashTags;
     @Transient
--- blog-microservice/src/main/java/com/blog/entity/Review.java
+++ blog-microservice/src/main/java/com/blog/entity/Review.java
@@ -1,5 +1,6 @@
 package com.blog.entity;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,10 +15,13 @@ public class Review {
 
     @Id
     private String id;
-    private String postId;
-    private String mobileNumber;
-    private Long views;     // It Go to blogDetails entity class TODO
+    @NotNull
+    private String userId;
+    @NotNull
+    private String blogId;
+    @NotNull
     private Integer rating;
+    @NotNull
     private String comment;
 
 }
