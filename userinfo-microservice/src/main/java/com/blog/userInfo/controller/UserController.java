5005738252c4ddc69c38388c4db40ff448ff584f
jasthirajaram <rajaram.jasthi@motivitylabs.com>
2023-06-27 13:00:32 +0530

    added configuration application.yml file

--- userinfo-microservice/src/main/java/com/blog/userInfo/controller/UserController.java
+++ userinfo-microservice/src/main/java/com/blog/userInfo/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.blog.userInfo.entity.UserDetails;
 import com.blog.userInfo.service.UserService;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
