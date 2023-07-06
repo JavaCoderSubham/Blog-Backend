3d526615017a5f06886437d180e0e4be584d71db
Rajaram Jasthi <jrrchowdary@gmail.com>
2023-07-06 18:32:26 +0530

    added logs and comments in admin MS

--- admin-microservice/src/main/java/com/blog/admin/controller/AdminController.java
+++ admin-microservice/src/main/java/com/blog/admin/controller/AdminController.java
@@ -21,108 +21,186 @@ import com.blog.admin.entity.Admin;
 import com.blog.admin.entity.AdminDto;
 import com.blog.admin.service.AdminService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	private AdminService adminService;
-	
+
+	//==================Create Admin Method====================
 	@PostMapping("/create")
 	public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+		log.info("==============Create Admin Started===============");
+		log.info("create(Admin) -> | Admin : {}", admin);
 		Admin save = adminService.createAdmin(admin);
+		log.info("create(Admin) -> | Admin after service : {}", admin);
+		log.info("==============Create Admin Ended===============");
 		return new ResponseEntity<Admin>(save, HttpStatus.CREATED);
 	}
+
+	//========================Find Admin Methods===========================
 	
 	@GetMapping("/findall")
 	public ResponseEntity<List<AdminDto>> findAllAdmins() {
+		log.info("=============Find All Admins Method Strated==============");
+		log.info("findAllAdmins() -> | Admin : ");
 		List<AdminDto> admins = adminService.findAllAdmins();
+		log.info("findAllAdmins() -> | Admin After Service : {}", admins);
+		log.info("=============Find All Admins Method Ended==============");
 		return new ResponseEntity<List<AdminDto>>(admins, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbyemail/{email}")
 	public ResponseEntity<AdminDto> findAdminByEmail(@PathVariable String email) {
+		log.info("============Find Admin By Email Started============");
+		log.info("findAdminByEmail() -> | Email : {}", email);
 		AdminDto admin = adminService.findAdminByEmail(email);
+		log.info("findAdminByEmail() -> | Admin By Email After Service : {}", email);
+		log.info("============Find Admin By Email Ended==============");
 		return new ResponseEntity<AdminDto>(admin, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbymobile/{mobile}")
 	public ResponseEntity<AdminDto> findAdminByMobile(@PathVariable String mobile) {
+		log.info("============Find Admin By Mobile Number Started============");
+		log.info("findAdminByMobile() -> | Mobile Number : {}", mobile);
 		AdminDto admin = adminService.findAdminByMobileNumber(mobile);
+		log.info("findAdminByMobile() -> | Admin By Mobile Number After Service : {}", admin);
+		log.info("============Find Admin By Mobile Number Ended==============");
 		return new ResponseEntity<AdminDto>(admin, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbyaadhar/{aadhar}")
 	public ResponseEntity<AdminDto> findAdminByAadhar(@PathVariable String aadhar) {
+		log.info("============Find Admin By Aadhar Started============");
+		log.info("findAdminByAadhar() -> | Aadhar : {}", aadhar);
 		AdminDto admin = adminService.findAdminByAadhar(aadhar);
+		log.info("findAdminByAadhar() -> | Admin By Aadhar After Service : {}", admin);
+		log.info("============Find Admin By Aadhar Ended============");
 		return new ResponseEntity<AdminDto>(admin, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbycity/{city}")
 	public ResponseEntity<List<AdminDto>> findAdminByCity(@PathVariable String city) {
+		log.info("============Find Admins By City Started============");
+		log.info("findAdminByCity() -> | City : {}", city);
 		List<AdminDto> admins = adminService.findAdminByAddressCity(city);
+		log.info("findAdminByCity() -> |Admins By City After Service : {}", admins);
+		log.info("============Find Admins By City Ended============");
 		return new ResponseEntity<List<AdminDto>>(admins, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbypincode/{pincode}")
 	public ResponseEntity<List<AdminDto>> findAdminByPincode(@PathVariable Integer pincode) {
+		log.info("============Find Admins By Pincode Started============");
+		log.info("findAdminByPincode() -> | Pincode : {}", pincode);
 		List<AdminDto> admins = adminService.findAdminByAddressPincode(pincode);
+		log.info("findAdminByPincode() -> |Admins By Pincode After Service : {}", admins);
+		log.info("============Find Admins By Pincode Ended============");
 		return new ResponseEntity<List<AdminDto>>(admins, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/findbystate/{state}")
 	public ResponseEntity<List<AdminDto>> findAdminByState(@PathVariable String state) {
-		List<AdminDto> admins = adminService.findAdminByAddressCity(state);
+		log.info("============Find Admins By State Started============");
+		log.info("findAdminByState() -> | State : {}", state);
+		List<AdminDto> admins = adminService.findAdminByAddressState(state);
+		log.info("findAdminByState() -> |Admins By State After Service : {}", admins);
+		log.info("============Find Admins By State Ended============");
 		return new ResponseEntity<List<AdminDto>>(admins, HttpStatus.OK);
 	}
+
+	//========================Change Admin Password Method================================
 	
 	@PutMapping("/changepassword/{id}")
-	public ResponseEntity<	Map<String, String>> changePassword(@PathVariable String email, @RequestParam String newPassword) {
+	public ResponseEntity<Map<String, String>> changePassword(@PathVariable String email,
+			@RequestParam String newPassword) {
+		log.info("=============Change Password Started==============");
+		log.info("changePassword() -> | Email : {}", email);
+		log.info("vhangePassword() -> | New Password : {}", newPassword);
 		adminService.changePassword(email, newPassword);
+		log.info("vhangePassword() -> | New Password After Service : {}", newPassword);
 		Map<String, String> map = new HashMap<>();
 		map.put("changePassword", "You have changed your password successfully");
-		return new ResponseEntity<Map<String,String>>(map, HttpStatus.OK);
+		log.info("============Change Password Ended  ==========");
+		return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
 	}
+
+	//===========================Update Admin methods===============================
 	
 	@PutMapping("/updatebyid/{id}")
 	public ResponseEntity<AdminDto> updateAdminById(@PathVariable String id, @RequestBody AdminDto admin) {
+		log.info("==============Update Admin By Id Started================");
+		log.info("updateAdminById() -> | Admin Id : {}", id);
+		log.info("updateAdminById() -> | Admin Before Service {}: ", admin);
 		AdminDto updatedAdmin = adminService.updateAdminById(id, admin);
+		log.info("updateAdminById() -> | Admin After Update : {}", admin);
+		log.info("==============Update Admin By Id Ended==================");
 		return new ResponseEntity<AdminDto>(updatedAdmin, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/updatebyemail/{email}")
 	public ResponseEntity<AdminDto> updateAdminByEmail(@PathVariable String email, @RequestBody AdminDto admin) {
+		log.info("==============Update Admin By Email Started================");
+		log.info("updateAdminByEmail() -> | Admin Email : {}", email);
+		log.info("updateAdminByEmail() -> | Admin Before Service {}: ", admin);
 		AdminDto updatedAdmin = adminService.updateAdminByEmail(email, admin);
+		log.info("updateAdminByEmail() -> | Admin After Update : {}", admin);
+		log.info("==============Update Admin By Email Ended==================");
 		return new ResponseEntity<AdminDto>(updatedAdmin, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/updatebymobile/{mobileNumber}")
-	public ResponseEntity<AdminDto> updateAdminByMobileNumber(@PathVariable String mobileNumber, @RequestBody AdminDto admin) {
+	public ResponseEntity<AdminDto> updateAdminByMobileNumber(@PathVariable String mobileNumber,
+			@RequestBody AdminDto admin) {
+		log.info("==============Update Admin By Mobile Number Started================");
+		log.info("updateAdminByMobile() -> | Admin Mobile : {}", mobileNumber);
+		log.info("updateAdminByMobile() -> | Admin Before Service {}: ", admin);
 		AdminDto updatedAdmin = adminService.updateAdminByMobileNumber(mobileNumber, admin);
+		log.info("updateAdminByMobile() -> | Admin After Update : {}", admin);
+		log.info("==============Update Admin By Mobile Number Ended==================");
 		return new ResponseEntity<AdminDto>(updatedAdmin, HttpStatus.CREATED);
 	}
+
+	//===========================Detele Admin Methods=========================
 	
 	@DeleteMapping("deletebyid/{id}")
 	public ResponseEntity<Map<String, String>> deleteAdminById(@PathVariable String id) {
+		log.info("===============Delete Admin By Id Started=================");
+		log.info("deleteAdminById() -> | Delete Admin Id : {}", id);
 		adminService.deleteAdminById(id);
+		log.info("deleteAdminById() -> | Deleted Admin Id : {}", id);
 		Map<String, String> map = new HashMap<>();
 		map.put("deleteAdmin", "You have deleted successfully with id " + id);
-		return new ResponseEntity<Map<String,String>>(map, HttpStatus.OK);
+		log.info("===============Delete Admin By Id Ended====================");
+		return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("deletebyemail/{email}")
 	public ResponseEntity<Map<String, String>> deleteAdminByEmail(@PathVariable String email) {
+		log.info("===============Delete Admin By Email Started=================");
+		log.info("deleteAdminByEmail() -> | Delete Admin Email : {}", email);
 		adminService.deleteAdminByEmail(email);
+		log.info("deleteAdminByEmail() -> | Deleted Admin Email : {}", email);
 		Map<String, String> map = new HashMap<>();
 		map.put("deleteAdmin", "You have deleted successfully with email " + email);
-		return new ResponseEntity<Map<String,String>>(map, HttpStatus.OK);
+		log.info("===============Delete Admin By Email Ended====================");
+		return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("deletebymobile/{mobile}")
 	public ResponseEntity<Map<String, String>> deleteAdminByMobileNumber(@PathVariable String mobileNumber) {
+		log.info("===============Delete Admin By Mobile Number Started=================");
+		log.info("deleteAdminByMobile() -> | Delete Admin Mobile : {}", mobileNumber);
 		adminService.deleteAdminByMobileNumber(mobileNumber);
+		log.info("deleteAdminByMobile() -> | Deleted Admin Mobile : {}", mobileNumber);
 		Map<String, String> map = new HashMap<>();
 		map.put("deleteAdmin", "You have deleted successfully with mobile number " + mobileNumber);
-		return new ResponseEntity<Map<String,String>>(map, HttpStatus.OK);
+		log.info("===============Delete Admin By Mobile Number Ended====================");
+		return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
 	}
 }
--- admin-microservice/src/main/java/com/blog/admin/service/AdminServiceImpl.java
+++ admin-microservice/src/main/java/com/blog/admin/service/AdminServiceImpl.java
@@ -9,94 +9,138 @@ import com.blog.admin.entity.Admin;
 import com.blog.admin.entity.AdminDto;
 import com.blog.admin.entity.dao.AdminDao;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class AdminServiceImpl implements AdminService {
 
 	@Autowired
 	private AdminDao adminDao;
 	
+	//========================Create Admin Method===========================
+	
 	@Override
 	public Admin createAdmin(Admin admin) {
+		log.info("create(Admin) -> | Subscribe : {}",admin);
 		Admin save = adminDao.createAdmin(admin);
+		log.info("create(Admin) -> | After execute Dao Create : {}",save);
 		return save;
 	}
 	
+	//========================Find Admin Methods==============================
+	
 	@Override
 	public List<AdminDto> findAllAdmins() {
-		return adminDao.findAllAdmins();
+		log.info("getAll() -> | ");
+		List<AdminDto> admins = adminDao.findAllAdmins();
+		log.info("getAll() -> | List Admins : {}",admins);
+		return admins;
 	}
-
+	
 	@Override
 	public AdminDto findAdminByEmail(String email) {
+		log.info("findAdminByEmail() -> | Email : {}", email);
 		AdminDto admin = adminDao.findAdminByEmail(email);
+		log.info("findAdminByEmail() -> | Admin By Email : {}", admin);
 		return admin;
 	}
 
 	@Override
 	public AdminDto findAdminByAadhar(String aadharNumber) {
+		log.info("findAdminByAadhar() -> | Aadhar Number : {}", aadharNumber);
 		AdminDto admin = adminDao.findAdminByAadhar(aadharNumber);
+		log.info("findAdminByAadhar() -> | Admin By Aadhar Number : {}", admin);
 		return admin;
 	}
 
 	@Override
 	public List<AdminDto> findAdminByAddressCity(String city) {
+		log.info("findAdminByAddressCity() -> | City : {}", city);
 		List<AdminDto> admins = adminDao.findAdminByAddressCity(city);
+		log.info("findAdminByAddressCity() -> | Admins By city : {}", admins);
 		return admins;
 	}
 
 	@Override
 	public List<AdminDto> findAdminByAddressState(String state) {
+		log.info("findAdminByAddressState() -> | State : {}", state);
 		List<AdminDto> admins = adminDao.findAdminByAddressCity(state);
+		log.info("findAdminByAddressState() -> | Admins By State : {}", admins);
 		return admins;
 	}
 
 	@Override
 	public List<AdminDto> findAdminByAddressPincode(Integer pincode) {
+		log.info("findAdminByAddressPincode() -> | Pincode : {}", pincode);
 		List<AdminDto> admins = adminDao.findAdminByAddressPincode(pincode);
+		log.info("findAdminByAddressPincode() -> | Admins By Pincode : {}", admins);
 		return admins;
 	}
 
 	@Override
 	public AdminDto findAdminByMobileNumber(String mobileNumber) {
+		log.info("findAdminByMobileNumber() -> | Mobile Number : {}", mobileNumber);
 		AdminDto admin = adminDao.findAdminByMobileNumber(mobileNumber);
+		log.info("findAdminByMobileNumber() -> | Admin By Mobile Number : {}", mobileNumber);
 		return admin;
 	}
 
+	//==========================Change Admin Password Method=======================
+	
 	@Override
 	public void changePassword(String email, String newPassword) {
+		log.info("changePassword() -> | Before Change Password : ");
 		adminDao.changePassword(email, newPassword);
+		log.info("changePassword() -> | After Change Password : ");
 	}
 
+	//===========================Update Admin Methods========================
+	
 	@Override
 	public AdminDto updateAdminById(String id, AdminDto admin) {
+		log.info("updateAdminById() -> | Before Update By Id : {}", admin);
 		AdminDto updatedAdmin = adminDao.updateAdminById(id, admin);
+		log.info("updateAdminById() -> | After Update By Id : {}", admin);
 		return updatedAdmin;
 	}
 
 	@Override
 	public AdminDto updateAdminByEmail(String email, AdminDto admin) {
+		log.info("updateAdminByEmail() -> | Before Update By Email : {}", admin);
 		AdminDto updatedAdmin = adminDao.updateAdminByEmail(email, admin);
+		log.info("updateAdminByEmail() -> | After Update By Email : {}", admin);
 		return updatedAdmin;
 	}
 
 	@Override
 	public AdminDto updateAdminByMobileNumber(String mobileNumber, AdminDto admin) {
+		log.info("updateAdminByMobileNumber() -> | Before Update By Mobile Number : {}", admin);
 		AdminDto updatedAdmin = adminDao.updateAdminByMobileNumber(mobileNumber, admin);
+		log.info("updateAdminByMobileNumber() -> | After Update By Mobile Number : {}", admin);
 		return updatedAdmin;
 	}
 
+	//=========================Delete Admin Methods==============================
+	
 	@Override
 	public void deleteAdminByEmail(String email) {
+		log.info("deleteAdminByEmail() -> | Delete By Email : {}", email);
 		adminDao.deleteAdminByEmail(email);
+		log.info("deleteAdminByEmail() -> | Deleted By Email : {}", email);
 	}
 
 	@Override
 	public void deleteAdminById(String id) {
+		log.info("deleteAdminByEmail() -> | Delete By Id : {}", id);
 		adminDao.deleteAdminById(id);
+		log.info("deleteAdminByEmail() -> | Delete By Id : {}", id);
 	}
 
 	@Override
 	public void deleteAdminByMobileNumber(String mobileNumber) {
+		log.info("deleteAdminByEmail() -> | Delete By Mobile Number : {}", mobileNumber);
 		adminDao.deleteAdminByMobileNumber(mobileNumber);
+		log.info("deleteAdminByEmail() -> | Delete By Mobile Number : {}", mobileNumber);
 	}
 }
