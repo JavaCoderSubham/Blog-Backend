bcaa429e3657022e4f7b3d8e1597953ba14d34a2
Rajaram Jasthi <jrrchowdary@gmail.com>
2023-07-11 14:58:09 +0530

    added eureka all MS registered

--- userinfo-microservice/src/main/java/com/blog/userInfo/UserinfoMicroserviceApplication.java
+++ userinfo-microservice/src/main/java/com/blog/userInfo/UserinfoMicroserviceApplication.java
@@ -3,8 +3,10 @@ package com.blog.userInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableEurekaClient
 public class UserinfoMicroserviceApplication {
 
 	public static void main(String[] args) {
